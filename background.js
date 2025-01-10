594d80b0136b6160d2c7af99c5019c344fa6b912
m00nbyt3 <moon_byte@protonmail.com>
2025-01-10 09:11:46 +0100

    first commit

---
+++ background.js
@@ -0,0 +1,4 @@
+chrome.action.onClicked.addListener(() => {
+  const url = "https://dominioshdfull.com/"; // Cambia esto a la URL que desees
+  chrome.tabs.create({ url });
+});
---
+++ main.js
@@ -0,0 +1,22 @@
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+
+//Assures all the web elements have finished loading
+async function main()
+{
+	try
+	{
+		await sleep(10);
+		window.location.href = document.getElementsByClassName("btn btn-lg btn-success d-block")[0].href;
+	}
+	catch
+	{
+		await sleep(1000);
+		window.location.href = document.getElementsByClassName("btn btn-lg btn-success d-block")[0].href;
+	}
+	
+}
+
+main();
